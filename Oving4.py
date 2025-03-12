713118fb30ed4ec16fc0d67d56c30b78fa550f48
Markus Hatlo <marhat182@gmail.com>
2025-03-12 08:50:46 +0100

    husker ikke

--- Oving4.py
+++ Oving4.py
@@ -1,25 +1,28 @@
 import scipy.io
 import numpy as np
 import scipy.signal as sg
+import matplotlib.pyplot as plt
 
 # Load the .mat file
 file_path = "XW_Sample_Sq39_farfield(1).mat"
 dataValues = scipy.io.loadmat(file_path)
-print(dataValues.keys())
 
 # Extract required variables
-u = np.float(dataValues['u'].squeeze)
-nu = np.float(dataValues['nu'])
-fs = np.float(dataValues['fs'])
+u = np.float64(dataValues['u'].squeeze())
+nu = (dataValues['nu']).item()
+fs = (dataValues['fs']).item()
+
+#1
 
 fc = fs/2.5
+fc_new = 0
 LPF_order = 7
 k = 0
 difference = 1
 
 
 def calcDissipationRate(u,u_prime,U_mean):
-    dt = 1.0 / fc
+    dt = 1.0 / fs
 
     du_dt = np.gradient(u_prime, dt)  
 
@@ -29,19 +32,52 @@ def calcDissipationRate(u,u_prime,U_mean):
 
     return 15.0 * nu * mean_du_dx_sq
 
-
-while difference < 0.01 or k == 9:
+#print(fc)
+while difference > 0.01 or k == 9:
     b, a = sg.butter(LPF_order, fc*2/fs, btype='lowpass', output='ba')
     u_filt = sg.filtfilt(b,a,u)
-    U_filt = np.mean(u)
-    up_filt = u - U_filt
+    U_filt = np.mean(u_filt)
+    up_filt = u_filt - U_filt
     dissipation = calcDissipationRate(u_filt,up_filt,U_filt)
-    f_eta = (nu**3/dissipation)**(1/4)
-    kolmogorov_frequency = U_filt/(2*np.pi*f_eta)
-    fc_new = 1.1*f_eta
+    eta = (nu**3/dissipation)**(1/4)
+    kolmogorov_frequency = U_filt/(2*np.pi*eta)
+    fc_new = 1.1*kolmogorov_frequency
     difference = abs(fc-fc_new)
     fc = fc_new
     k += 1
-    print(k)
 
-print(fc_new)
+#print(round(fc))
+
+#2
+BIN = 14
+
+Freq, E_f = sg.welch(up_filt, fs, nperseg=2**(BIN+1))
+
+#3
+E_k = E_f*U_filt/(2*np.pi)
+k_space = 2*np.pi*fc
+
+
+#4
+y_aksen = E_k*eta/nu**2
+x_aksen = k_space*Freq
+
+
+plt.figure(figsize=(10, 5))
+plt.loglog(x_aksen,y_aksen)
+plt.loglog(x_aksen,1e15*x_aksen**(-5/3))
+plt.ylabel("E_k*eta/nu**2")
+plt.xlabel('K_eta')
+plt.ylim(1e-6)
+plt.title('E_k*eta/nu**2 vs k_space*f')
+plt.show()
+
+
+# maxLags = 40000
+# Buu, lags,mode = np.correlate(up_filt,maxLags,'full')
+# rs = lags * (U_filt/fs)
+
+# plt.loglog(rs, Buu)
+# plt.xlabel('Buu')
+# plt.ylabel('rs [m]')
+# plt.show()
