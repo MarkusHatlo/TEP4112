ec553c899b6b2ba2de5788d1747eb25c4361b7fc
Markus Hatlo <marhat182@gmail.com>
2025-03-26 08:49:24 +0100

    husket ikke a commite

--- LabCode.py
+++ LabCode.py
@@ -72,15 +72,46 @@ def plot_autocorrelation():
     plt.title(f'Autocorrelation of u′ (log scale on x-axis)- File: {i}M')
     plt.show()
 
+L11_list = []
+decades_list = []
+
 def calc_decades():
-    zero_crossing_indices = np.where(Buu <= 0)[0]
-    first_zero_idx = zero_crossing_indices[0]
+    print(f"Min Buu: {np.min(Buu)}, Max Buu: {np.max(Buu)}")
+    if i == 57:
+        zero_crossing_indices = np.where(Buu <= 0.006)[0]
+        first_zero_idx = zero_crossing_indices[0]
+    elif i == 61:
+        zero_crossing_indices = np.where(Buu <= 0.0129)[0]
+        first_zero_idx = zero_crossing_indices[0]
+    elif i == 70:
+        zero_crossing_indices = np.where(Buu <= 0.009)[0]
+        first_zero_idx = zero_crossing_indices[0]
+    elif i == 75:
+        zero_crossing_indices = np.where(Buu <= 0.005)[0]
+        first_zero_idx = zero_crossing_indices[0]
+    else:
+        zero_crossing_indices = np.where(Buu <= 0)[0]
+        first_zero_idx = zero_crossing_indices[0]
     L11 = np.trapz(Buu[:first_zero_idx+1], rs[:first_zero_idx+1])
 
     decades = np.log10(L11/eta)
     print(f"L11 {L11}")
     print(f"Eta {eta}")
     print(f"Decades {decades}")
+
+    L11_list.append(float(L11))
+    decades_list.append(float(decades))
+
+    if len(L11_list) == 13:
+            print(f"L11 {L11_list}")
+            print(f"Eta {eta_list}")
+            print(f"Decades {decades_list}")
+    #     plt.figure(figsize=(10, 5))
+    #     plt.plot([25,29,34,38,43,47,52,57,61,66,70,75,80],L11_list)
+    #     plt.xlabel('Streamwise position from the turbulence grid [M]')
+    #     plt.ylabel('Integral length scale [m]')
+    #     plt.title('Integral length scale at each streamwise position')
+    #     plt.show()
 
 decay = []
 decay_position = []
@@ -107,9 +138,9 @@ def plot_statistics():
     if len(u_filt_kde_list) == 3:
 
         plt.figure(figsize=(10, 6))
-        plt.plot(x_range, u_filt_kde_list[0], 'r-', linewidth=2, label='Turbulence Grid', color = 'red')
-        plt.plot(x_range, u_filt_kde_list[1], 'r-', linewidth=2, label='Turbulence Grid', color = 'blue')
-        plt.plot(x_range, u_filt_kde_list[2], 'r-', linewidth=2, label='Turbulence Grid', color = 'green')
+        plt.plot(x_range, u_filt_kde_list[0], 'r-', linewidth=2, label='25M', color = 'red')
+        plt.plot(x_range, u_filt_kde_list[1], 'r-', linewidth=2, label='52M', color = 'blue')
+        plt.plot(x_range, u_filt_kde_list[2], 'r-', linewidth=2, label='80M', color = 'green')
         plt.plot(x_range, gaussian_pdf, 'k--', linewidth=2, label='Gaussian (S=0, K=3)')
         plt.xlabel(r'$u\' / \sigma$')
         plt.ylabel('Probability Density')
@@ -119,11 +150,12 @@ def plot_statistics():
         plt.xlim(-4, 4)
         plt.show()
 
+eta_list = []
 
 # Load the .mat file
-#for i in [25,29,34,38,43,47,52,57,61,66,70,75,80]:
-for i in [25,52,80]:
-#for i in [25]:
+for i in [25,29,34,38,43,47,52,57,61,66,70,75,80]:
+#for i in [25,52,80]:
+# for i in [70,75]:
     file_path = f"{i}M_processed.mat"
     dataValues = scipy.io.loadmat(fr"Group 8\processed\{file_path}")
 
@@ -159,15 +191,17 @@ for i in [25,52,80]:
         fc = fc_new
         n += 1
 
-    maxLags = 40000
-    acf_result = sm.tsa.acf(up_filt, nlags=maxLags, fft=True)
-    Buu = acf_result
-    lags = np.arange(len(Buu))
-    rs = lags * (U_filt/fs)
+    eta_list.append(float(eta))
 
-    u_filt_std = np.std(up_filt)
+    # maxLags = 40000
+    # acf_result = sm.tsa.acf(up_filt, nlags=maxLags, fft=True)
+    # Buu = acf_result
+    # lags = np.arange(len(Buu))
+    # rs = lags * (U_filt/fs)
 
-    # plot_spectra()
+    # u_filt_std = np.std(up_filt)
+
+    #plot_spectra()
 
     # plot_autocorrelation()
 
@@ -196,14 +230,15 @@ scaling_coefficient = first_value / (first_position**(-1.14))
 powerlaw = [scaling_coefficient * (i**(-1.14)) for i in decay_position]
 
 
-def power_law(x, A, n):
-    return A * x**(-n)
+def power_law(x, A, n, x0):
+    return A * (x-x0)**(-n)
+
 
 params, params_covariance = curve_fit(power_law, decay_position, decay)
-A_fit, n_fit = params
-powerlaw_fit = power_law(np.array(decay_position), A_fit, n_fit)
+A_fit, n_fit, x0_fit = params
+powerlaw_fit = power_law(np.array(decay_position), A_fit, n_fit, x0_fit)
 
-print(f"Fitted parameters: A = {A_fit:.4f}, n = {n_fit:.4f}")
+print(f"Fitted parameters: A = {A_fit:.4f}, n = {n_fit:.4f} x0 = {x0_fit:.4g}")
 print(f'This is the decay numbers A = {scaling_coefficient:.4g} deacy = {decay} and position = {decay_position}')
 
 std_curve_fit = np.sqrt(np.diag(params_covariance))
